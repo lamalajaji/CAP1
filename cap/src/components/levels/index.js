92fd18b968f458c81800fcd64f21dff5a863fc1b
lamalajaji <Lama.Mohammed.elq.js@tuwaiq.edu.sa>
2021-11-06 20:07:30 +0300

    modified levels page

--- cap/src/components/levels/index.js
+++ cap/src/components/levels/index.js
@@ -2,21 +2,40 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import "./style.css";
 
-export default  Gamelevel = () => {
-    return (
-        <div className="levelContener">
-                       <img id="backGroundImg" src="https://i.pinimg.com/originals/78/cc/6e/78cc6e42b85291f8edb4c9ca7a7a1d60.gif" alt=""/>
-                       <div className="contentDiv">
-            <h4 className="levelHeader"> SELECT LEVEL </h4>
-              <button className="levelBtn"> <Link className="levelLink" to="/Gamelevel/Easy"> Easy </Link>  </button>
-              <button className="levelBtn"> <Link className="levelLink" to="/Gamelevel/Medium"> Medium </Link>  </button>
-              <button className="levelBtn"> <Link className="levelLink" to="/Gamelevel/Hard"> Hard </Link>  </button>
-
-</div>
-
-
-        </div>
-    )
-}
+export default Gamelevel = () => {
+  return (
+    <div className="levelContener">
+      <img
+        id="backGroundImg"
+        src="https://i.pinimg.com/564x/ef/37/cd/ef37cdd4aa5f17ede3d10050e8968df1.jpg"
+        alt=""
+      />
+      <div className="contentDiv">
+        <h4 className="levelHeader"> SELECT LEVEL </h4>
+        <button className="levelBtn">
+          {" "}
+          <Link className="levelLink" to="/Gamelevel/Easy">
+            {" "}
+            Easy{" "}
+          </Link>{" "}
+        </button>
+        <button className="levelBtn">
+          {" "}
+          <Link className="levelLink" to="/Gamelevel/Medium">
+            {" "}
+            Medium{" "}
+          </Link>{" "}
+        </button>
+        <button className="levelBtn">
+          {" "}
+          <Link className="levelLink" to="/Gamelevel/Hard">
+            {" "}
+            Hard{" "}
+          </Link>{" "}
+        </button>
+      </div>
+    </div>
+  );
+};
 
 
