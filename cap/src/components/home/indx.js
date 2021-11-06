200df9b07b86ef4d5fd4d4915e45a986e8b9c48f
lamalajaji <Lama.Mohammed.elq.js@tuwaiq.edu.sa>
2021-11-06 20:06:00 +0300

    edit homepage

--- cap/src/components/home/indx.js
+++ cap/src/components/home/indx.js
@@ -8,7 +8,7 @@ export default Home = () => {
     <div>
       <img
         id="backGroundImg"
-        src="https://i.pinimg.com/originals/78/cc/6e/78cc6e42b85291f8edb4c9ca7a7a1d60.gif"
+        src="https://i.pinimg.com/564x/ef/37/cd/ef37cdd4aa5f17ede3d10050e8968df1.jpg"
         alt=""
       />
       <div className="describeItem">
@@ -21,7 +21,7 @@ export default Home = () => {
           </Link>{" "}
         </button>
         <div></div>
-        <button className="homeBtn">
+        <button className="homeBtn" id="lastB">
           {" "}
           <Link className="btnLink" to="/Instruction">
             {" "}
@@ -32,5 +32,6 @@ export default Home = () => {
     </div>
   );
 };
+
 
 
